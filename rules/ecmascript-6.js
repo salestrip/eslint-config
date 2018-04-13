8378b87b4576e07d888cd75254ceea019852f828
Sid Wood <sidwood@me.com>
2018-04-13 07:29:56 +0100

    Add ecmascript 6 rules

---
+++ rules/ecmascript-6.js
@@ -0,0 +1,37 @@
+'use strict'
+
+module.exports = {
+  rules: {
+    'arrow-body-style': ['error', 'always'],
+    'arrow-parens': ['error', 'always'],
+    'arrow-spacing': ['error', {after: true, before: true}],
+    'constructor-super': 'error',
+    'generator-star-spacing': ['error', {after: false, before: true}],
+    'no-class-assign': 'error',
+    'no-confusing-arrow': 'error',
+    'no-const-assign': 'error',
+    'no-dupe-class-members': 'error',
+    'no-duplicate-imports': 'error',
+    'no-new-symbol': 'error',
+    'no-restricted-imports': 'off',
+    'no-this-before-super': 'error',
+    'no-useless-computed-key': 'error',
+    'no-useless-constructor': 'error',
+    'no-useless-rename': 'error',
+    'no-var': 'error',
+    'object-shorthand': ['error', 'always'],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'prefer-destructuring': 'error',
+    'prefer-numeric-literals': 'error',
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'require-yield': 'error',
+    'rest-spread-spacing': ['error', 'never'],
+    'sort-imports': 'error',
+    'symbol-description': 'error',
+    'template-curly-spacing': ['error', 'never'],
+    'yield-star-spacing': ['error', {after: false, before: true}]
+  }
+}
