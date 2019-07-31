49467182b081e3ccb472c3cae6aa28656f686171
Sid Wood <sidwood@me.com>
2019-07-31 07:51:23 +0100

    Add quote and quote-props rules to formatter
    The formatter does not load our eslint rules so we need to add these to
    undo changes made by prettier that are not compatible with our eslint
    settings

--- bin/format.sh
+++ bin/format.sh
@@ -15,6 +15,8 @@ format() {
     --env node,es6 \
     --parser-options ecmaVersion:2019 \
     --rule 'brace-style:[2,stroustrup],indent:[2, 2,{SwitchCase:1}]' \
+    --rule 'quote-props:[2,consistent-as-needed]' \
+    --rule 'quotes:[error,single,avoid-escape]' \
     $*
 }
 
